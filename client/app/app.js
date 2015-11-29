e76f4f0c603b9e47d8c417fce88eecf44f4e1ed7
Patrick <pcnicklaus@gmail.com>
2015-11-29 16:01:17 -0700

    hacking away at learning angular material

--- client/app/app.js
+++ client/app/app.js
@@ -5,7 +5,9 @@ angular.module('materialDesignTwoApp', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ngAnimate',
+  'ngMaterial'
 ])
   .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
