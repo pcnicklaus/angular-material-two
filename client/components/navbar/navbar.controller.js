f64ad83696ac0b94184389e1eb8097493898fa2a
Patrick <pcnicklaus@gmail.com>
2015-11-29 15:40:32 -0700

    initial checkin'

---
+++ client/components/navbar/navbar.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('materialDesignTwoApp')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.menu = [{
+      'title': 'Home',
+      'state': 'main'
+    }];
+
+    $scope.isCollapsed = true;
+  });
---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('materialDesignTwoApp')
+  .directive('navbar', function () {
+    return {
+      templateUrl: 'components/navbar/navbar.html',
+      restrict: 'E',
+      controller: 'NavbarCtrl'
+    };
+  });
